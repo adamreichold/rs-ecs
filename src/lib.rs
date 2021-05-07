0c02e7a88fcd8e904e704358505a3d10ffba115b
Martin Lange <martin.lange@ufz.de>
2021-05-07 15:17:52 +0200

    add documentation for crate and public members

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+//! A reasonably simple entity component system (ECS).
+//!
+//! The design is based on [hecs](https://github.com/Ralith/hecs) but it is not thread-safe
+//! and has a significantly reduced API surface.
+//!
+//! # Example
+//!
+//! ```
+//! # use rs_ecs::*;
+//!
+//! let mut world = World::new();
+//!
+//! let entity = world.alloc();
+//! world.insert(entity, (42_u32, true));
+//!
+//! let entity = world.alloc();
+//! world.insert(entity, (23_u32, "hello".to_string()));
+//!
+//! for number in Query::<&u32>::new().iter(&world) {
+//!     println!("{}", number);
+//! }
+//!
+//! for (_entity, number, string) in Query::<(&Entity, &u32, &String)>::new().iter(&world) {
+//!     println!("{}, {}", string, number);
+//! }
+//! ```
+
+#![warn(missing_docs)]
+
 mod archetype;
 mod query;
 mod resources;
